15f666ac979b356db213307a81d8715537f0753f
fdemian <demian.fc@gmail.com>
2017-05-04 15:41:27 -0300

    Agregar test para metodología.

---
+++ donde-invierto/src/test/java/dominio/MetodologiaTest.java
@@ -0,0 +1,52 @@
+package dominio;
+
+// JUnit
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertFalse;
+
+// Funciones de DateTime.
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+// Big Decimal
+import java.math.BigDecimal;
+
+public class MetodologiaTest 
+{
+  
+  Metodologia metodologia = null;   
+  
+  @Before
+  public void cargaMetodologia() 
+  {	
+ 	BigDecimal valor = new BigDecimal(4500);
+	
+    // Set cuenta.
+    LocalDate fechaInicio = LocalDate.parse("2007-01-01");
+	LocalDate fechaFin = LocalDate.parse("2008-01-01");
+	Periodo periodo = new Periodo(fechaInicio, fechaFin); 	 	 
+    Cuenta cuenta = new Cuenta("Ahorros", periodo, valor);	
+ 
+    Empresa empresa = new Empresa("Empresa Test");	
+    Condicion condicion = new Condicion(cuenta, empresa, valor);
+	
+	metodologia = new Metodologia(condicion);	
+  }
+
+  
+  @Test
+  public void testConvieneInvertir()
+  {		    
+	Empresa empresa = new Empresa("Empresa Test");
+
+    LocalDate fechaInicio = LocalDate.parse("2007-01-01");
+	LocalDate fechaFin = LocalDate.parse("2008-01-01");
+	Periodo periodo = new Periodo(fechaInicio, fechaFin); 	 
+  
+	boolean conviene = metodologia.convieneInvertir(empresa, periodo);
+	
+	assertFalse(conviene);
+  }
+  
+}
