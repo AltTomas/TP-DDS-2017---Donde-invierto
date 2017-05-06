2283def6c42c62bb4501d07cdf555153b11e2b6f
fdemian <demian.fc@gmail.com>
2017-05-06 00:23:35 -0300

    Agregar README con instrucciones para correr el proyecto. Ventana de prueba usando JavaFx.

--- docs/README.md
+++
@@ -1,5 +0,0 @@
-# Correr el programa
-
-1. mvn clean compile
-2. mvn package
-3. mvn exec:java
--- donde-invierto/src/main/java/dominio/Main.java
+++ donde-invierto/src/main/java/dominio/Main.java
@@ -1,9 +1,46 @@
 package dominio;
 
-public class Main
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application
 {
-	public static void main(String [] args)
-	{
-		System.out.println("UTN DDS - Donde Invierto");		
-	}	
+	@Override
+    public void start(final Stage primaryStage) {
+       
+        final Button button = new Button();
+        button.setText("Demo");       
+        button.setOnAction((ActionEvent event) -> {            
+            System.out.println("Donde Invierto: Funcionalidad no implementada");
+        });
+		        
+        final StackPane root = new StackPane();
+        
+		// Agregar los hijos de la ventana (botón).
+        root.getChildren().add(button);
+        
+		// Crear una escena.
+        final Scene scene = new Scene(root, 300, 250);
+        
+		// Titulo de la ventana.
+        primaryStage.setTitle("Donde Invierto");
+        primaryStage.setScene(scene);
+        
+		// Mostrar la ventana.
+        primaryStage.show();
+    }
+ 
+    /**
+     * Main function that opens the "Hello World!" window
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(final String[] arguments) {
+        launch(arguments);
+    }
 }
