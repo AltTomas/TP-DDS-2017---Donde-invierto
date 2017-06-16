6983b9bb0e1b8bff2021ae162e520d5d8d83beb5
Pablo Horak <pablo_horak@yahoo.com.ar>
2017-06-16 14:31:41 -0300

    CambiosParser

--- src/test/java/dominio/ParserTest.java
+++ src/test/java/dominio/ParserTest.java
@@ -2,24 +2,78 @@ package dominio;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-
+import java.io.*;
+import java.lang.*;
 // JUnit
 import org.junit.Test;
+import org.junit.Rule;
 import org.junit.Before;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
 
 import dominio.AnalizadorLexico;
 import helpers.FileToStringReader;
 
 public class ParserTest 
 {        
+
+@Rule
+public ExpectedException thrown = ExpectedException.none();
+
   @Test
-  public void testInstanciaAnalizadorLexico() throws ParseException, FileNotFoundException
+  public void testInstanciaAnalizadorLexicoArchivo() throws ParseException, FileNotFoundException
   {
-    String file = "C:/Projects/2017-mn-group-04/src/test/resources/indicador.txt";
-	FileInputStream input = new FileInputStream(file);
-	AnalizadorLexico analizador = new AnalizadorLexico();	
-	//analizador.main(input);
-  }  
+    String file = "src/test/resources/pruebaParser.json";
+    AnalizadorLexico parser = null;
+    boolean leer;
+try{
+    parser = new AnalizadorLexico(new FileInputStream(file));
+   }catch(FileNotFoundException e){
+   	System.out.println("El archivo "+file+" no se encuentra");
+	return ; 
+  }
+   while(leer=parser.Programa());	  
+
+  assertFalse(parser.Programa());
+    
+}
+
+@Test
+  public void testInstanciaAnalizadorLexicoCadena() throws ParseException, FileNotFoundException
+  {
+    String cadena1 = "\""+"formula\""+": "+"\"Operacion Neta+Capital\"";
+	Reader reader1 = new StringReader(cadena1);    
+	AnalizadorLexico parser=null;
+    System.out.println("Cadena Ingresada" + cadena1);
+
+ 	if( parser == null ) 
+		parser = new AnalizadorLexico(reader1);
+	else 
+		parser.ReInit(reader1);
+
+    assertTrue(parser.Programa());	  
+  
+   
+  }
+
+  @Test(expected = ParseException.class)
+    public void testInstanciaAnalizadorLexicoCadenaFalse() throws ParseException, FileNotFoundException
+    {
+      String cadena2 = "formula = a+b";
+    
+      Reader reader2 = new StringReader(cadena2);    
+  	AnalizadorLexico parser=null;
+
+ 	System.out.println("Cadena Ingresada" + cadena2);
+ 	if( parser == null ) 
+ 			parser = new AnalizadorLexico(reader2);
+ 	else 
+ 		parser.ReInit(reader2);
+      
+	    parser.Programa();
+   }     
+
+ 
+
+  
 }
